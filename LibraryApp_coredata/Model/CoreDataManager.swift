47dab3efe0164880197720f3c6521f2f1d6d67bc
elabell <elabell@laposte.net>
2019-04-01 17:45:08 +0200

    [FIX] Refactoring MVC

--- LibraryApp_coredata/Model/CoreDataManager.swift
+++ LibraryApp_coredata/Model/CoreDataManager.swift
@@ -21,7 +21,7 @@ class CoreDataManager {
     
     var delegate: CoreDataManagerDelegate?
     
-    var context : NSManagedObjectContext {
+    private var context : NSManagedObjectContext {
         return persistentContainer.viewContext
     }
     
@@ -59,6 +59,14 @@ class CoreDataManager {
         return container
     }()
     
+   
+    
+    // MARK: - Core Data getContext
+    
+    func getContext() -> NSManagedObjectContext {
+        return CoreDataManager.shared.context
+    }
+    
     // MARK: - Core Data Saving support
     
     func saveContext () {
@@ -75,14 +83,268 @@ class CoreDataManager {
         }
     }
     
-    // MARK: - Core
-    
-    
-    
-    // MARK: - C
-    
-    
- 
 }
+    
+    
+
+  // MARK: - extension ItemCore
+    extension CoreDataManager {
+       
+        
+  
+     //MARK: -  CoreData  initTablewithItems
+    
+    func initTablewithItems() -> [ItemCore]  {
+        //let task1 = ItemCore(text : "Finir le cours d'IOS")
+        // let task2 = ItemCore(text : "Mettre à jour XCode",_checked:true)
+        // let task3 = ItemCore(text : "Ma tache perso")
+        //  let task4 = ItemCore(text : "Home work Java",_checked:true)
+        
+        /*   let appDelegate = UIApplication.shared.delegate as! AppDelegate
+         
+         */
+        
+        var _tableItems : [ItemCore] = []
+        
+        let item = ItemCore(context: getContext())
+        
+        item.checked = true
+        
+        let context: NSManagedObjectContext = getContext()
+        let entity = NSEntityDescription.entity(forEntityName: "ItemCore" , in: context)
+        
+        // let newtask1   = NSManagedObject(entity: entity!, insertInto: context ) as! ItemCore
+        let newtask1   = ItemCore(entity: entity!, insertInto: context )
+        newtask1.setValue("Finir le cours d'IOS", forKey: "text")
+        
+        let newtask2   = NSManagedObject(entity: entity!, insertInto: context ) as! ItemCore
+        newtask2.setValue("Mettre à jour XCode", forKey: "text")
+        newtask2.setValue(true, forKey: "checked")
+        
+        let newtask3   = NSManagedObject(entity: entity!, insertInto: context )as! ItemCore
+        newtask3.setValue("Ma tache perso", forKey: "text")
+        
+        let newtask4   = NSManagedObject(entity: entity!, insertInto: context ) as! ItemCore
+        newtask4.setValue("Home work Java", forKey: "text")
+        newtask4.setValue(true, forKey: "checked")
+        
+        
+        //TODO insert into Coredata BBD
+        //Recoup of context from CoreDataManager
+        //let test = ItemCore(context: NSManagedObjectContext())
+        
+        _tableItems.append(newtask1 )
+        _tableItems.append(newtask3 )
+        _tableItems.append(newtask3 )
+        _tableItems.append(newtask4 )
+        
+        saveData ()
+        // nbItems = tableItems.count
+        
+        return _tableItems
+    }
+    
+    
+    //MARK: -  CoreData  save the context in the Database
+   
+    func saveData (){
+        do {
+            try getContext().save()
+        } catch{
+            print("Failed saving")
+        }
+        
+    }
+        
+       //MARK: -  CoreData
+        func createNewItem(txt : String? =  nil, changed : Bool? = nil ) -> ItemCore{
+            
+            
+        let context = getContext()
+        let entity = NSEntityDescription.entity(forEntityName: "ItemCore" , in: context)
+        
+        //  let item = ItemCore(context: self.getContext())//Item(_text: txt)
+        // tableItems =Array<Item>(repeating: Item, count: 9)
+        
+        let newitem   = ItemCore(entity: entity!, insertInto: context )
+        
+        newitem.setValue(txt, forKey: "text")
+            
+         
+           return newitem
+        }
+        
+    // MARK: - getItems
+    func getItems(predicate: NSPredicate? = nil,
+                      sortDescriptors: [NSSortDescriptor]? = nil,
+                      limit: Int = 100) -> [ItemCore] {
+            
+            
+            let fetchRequest = NSFetchRequest<ItemCore>()
+            
+            fetchRequest.predicate = predicate
+            
+            fetchRequest.sortDescriptors = sortDescriptors
+            
+            let _context:  NSManagedObjectContext  = CoreDataManager.shared.context
+            
+            return try!  _context.fetch(fetchRequest) // getContext().fetch(fetchRequest)
+            
+        }
+        
+     
+        //MARK: -  CoreData checkifCoreDataExists
+        
+        func checkIfCoreDataExists(text : String) -> Bool{
+            //predicate // Use NSPredicate to filter articlID in coredata
+            
+            //let fetchRequest = NSFetchRequest<ItemCore>()
+            
+            let request = NSFetchRequest<ItemCore>()
+            let predicate = NSPredicate(format: "text == %ld", text)
+            
+            request.predicate = predicate
+            //  let fetchResults = self.getContext().fetch(request) as? [ItemCore]
+            
+            request.fetchLimit = 1
+            
+            do{
+                
+                let context =  self.getContext()
+                let count = try context.count(for: request)
+                if(count == 0){
+                    // no matching object
+                    print("no present")
+                    return false
+                    
+                }
+                else{
+                    // at least one matching object exists
+                    print("one matching item found")
+                    return true
+                }
+            }
+            catch let error as NSError {
+                print("Could not fetch \(error), \(error.userInfo)")
+                return false
+            }
+            
+            
+            
+        }
+        
+   // MARK: -  CoreData fetchCoreData_fromContext
+        
+    func fetchCoreData_fromContext() -> [ItemCore]{
+            
+            var tableItems : [ItemCore] = []
+            
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "ItemCore")
+            
+            //it's any sql query ?
+            //request.predicate = NSPredicate(format: "text = %@","Home work Java")
+            // let context: NSManagedObjectContext = getContext()
+            // let entity = NSEntityDescription.entity(forEntityName: "ItemCore" , in: context)
+            
+            // let newrecord = NSEntityDescription.insertNewObject(forEntityName: "ItemCore", into: context) as NSManagedObject
+            // let timestamp = NSDate()
+            
+            
+            do {
+                let result =  try  getContext().fetch(request)  // context contient the same data which  array [ItemCore]
+                for data in result as! [NSManagedObject]{
+                    
+                    print(data.value(forKey: "text" ) as! String)
+                    
+                    //  let  _txt: String = data.value(forKey: "text" ) as! String
+                    //  let  _checked     = data.value(forKey: "checked" ) as? Bool
+                    //  let  _photo           = data.value(forKey: "photo" ) as? NSData
+                    
+                    tableItems.append(data as! ItemCore)
+                    
+                    
+                }
+                
+            } catch  {
+                print("Fetch Failed")
+            }
+            
+            return tableItems
+        }
+        
+        
+        
+        
+     //MARK: -  CoreData writeData_toTableItems
+        
+      func writeData_toTableItems(entityName: String ,tableItems : [ItemCore] ){
+            
+            // entityName = "ItemCore"
+            
+            let context: NSManagedObjectContext = getContext()
+            let entity = NSEntityDescription.entity(forEntityName: entityName , in: context)
+            
+            let newrecord = NSEntityDescription.insertNewObject(forEntityName: entityName, into: context) as NSManagedObject
+            let timestamp = NSDate()
+            
+            for item in tableItems{
+                
+                newrecord.setValue(item.text, forKey: "text") // EBE  comment recup Key generique ??
+                newrecord.setValue(item.checked, forKey: "checked")
+                newrecord.setValue(item.photo, forKey: "photo")
+                
+                do{
+                    try context.save()
+                    print("Saved successfully")
+                } catch _ {
+                    print("there was issue saving data!")
+                }
+            }
+            
+        }
+        
+    
+
+
+    
+}
+    
+   /*
+    func fetchfromCoreData2(){
+        
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "ItemCore")
+        
+        //it's any sql query ?
+        //request.predicate = NSPredicate(format: "text = %@","Home work Java")
+        let context: NSManagedObjectContext = getContext()
+        let entity = NSEntityDescription.entity(forEntityName: "ItemCore" , in: context)
+        
+        
+        do {
+            let result =  try  getContext().fetch(request)
+            for data in result as! [NSManagedObject]{
+                
+                print(data.value(forKey: "text" ) as! String)
+                let txt: String = data.value(forKey: "text" ) as! String
+                
+                if(txt != ""){
+                    let newtask  = NSManagedObject(entity: entity!, insertInto: context ) as! ItemCore
+                    newtask.setValue(txt, forKey: "text")
+                    newtask.setValue(true, forKey: "checked")
+                    
+                    tableItems.append(newtask)
+                    saveData () // Not sure if need it here
+                    
+                }
+                
+            }
+            
+        } catch  {
+            print("Fetch Failed")
+        }
+        
+        
+    } */
 
 
