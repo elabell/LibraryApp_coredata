7480fcb315e3228d85edbc74ad163f470c504506
ela <elabell@laposte.net>
2019-10-27 15:01:19 +0100

    [Add] add missing CoreDataManagerDelegate

--- LibraryApp_coredata/Model/CoreDataManager.swift
+++ LibraryApp_coredata/Model/CoreDataManager.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import CoreData
+import UIKit
 
 protocol CoreDataManagerDelegate {
     func didUpdate()
@@ -15,7 +16,7 @@ protocol CoreDataManagerDelegate {
 
 class CoreDataManager {
     
-     //add deegate here ?
+    //add delegate here ?
     
     static let shared = CoreDataManager()
     
@@ -79,32 +80,76 @@ class CoreDataManager {
         }
     }
 }
+
+
+
+// MARK: - Core
+extension CoreDataManager {
     
-    
-    
-    // MARK: - Core
-    extension CoreDataManager {
+    func  createNewItem(txt: String, ischecked: Bool? = true, image: UIImage? = nil,date: Date? = nil, cat: Category)-> ItemCore {
         
-        func  createNewItem(txt: String, ischecked: Bool? = true)-> ItemCore {
+        let context = self.getContext()
+        let entity = NSEntityDescription.entity(forEntityName: "ItemCore" , in: context)
+        
+        //  let item = ItemCore(context: self.getContext())//Item(_text: txt)
+        // tableItems =Array<Item>(repeating: Item, count: 9)
+        
+        let newitem   = ItemCore(entity: entity!, insertInto: context )
+       
+        //newitem.setValue(txt, forKey: "text")
+        //newitem.setValue( cat  , forKey: "withCat")
+        newitem.text = txt
+        newitem.deadline = date
+        newitem.withCat = cat
+        
+        if (image != nil) {
+           // let _image : UIImage =  UIImage(data:(image)!)!
+            let dataimage : Data = image!.pngData()!
+            newitem.photo = dataimage
             
-            let context = self.getContext()
-            let entity = NSEntityDescription.entity(forEntityName: "ItemCore" , in: context)
+        }else {
             
-            //  let item = ItemCore(context: self.getContext())//Item(_text: txt)
-            // tableItems =Array<Item>(repeating: Item, count: 9)
-            
-            let newitem   = ItemCore(entity: entity!, insertInto: context )
-            newitem.setValue(txt, forKey: "text")
-            
-            addItem(item: newitem)
-            saveData()
-            
-           return newitem
-            
-            
+          let _image = UIImage(named: "internet")
+            let dataimage : Data = _image!.pngData()!
+            newitem.photo = dataimage
         }
-   
-        func initTablewithItems(){
+        
+        addItem(item: newitem)
+        saveData()
+        
+        return newitem
+        
+        
+    }
+    
+    func  createNewSection(txt: String,image: UIImage? = nil) -> Category{
+        let context = self.getContext()
+        let entity = NSEntityDescription.entity(forEntityName: "Category" , in: context)
+        
+        //  let item = ItemCore(context: self.getContext())//Item(_text: txt)
+        // tableItems =Array<Item>(repeating: Item, count: 9)
+        
+        
+        let newitem   = Category(entity: entity!, insertInto: context )
+        newitem.setValue(txt, forKey: "name")
+        if (image != nil){
+            
+            let data : Data = image!.pngData()!
+            newitem.setValue(data, forKey: "image")
+        }else{
+            let image = UIImage(named: "shops")
+            let data : Data = image!.pngData()!
+             newitem.setValue(data, forKey: "image")
+        }
+        
+        addCategory(item: newitem)
+        saveData()
+        
+        return newitem
+        
+    }
+    
+    func initTablewithItems(){
         //let task1 = ItemCore(text : "Finir le cours d'IOS")
         // let task2 = ItemCore(text : "Mettre à jour XCode",_checked:true)
         // let task3 = ItemCore(text : "Ma tache perso")
@@ -113,7 +158,7 @@ class CoreDataManager {
         /*   let appDelegate = UIApplication.shared.delegate as! AppDelegate
          
          */
-       var tableItems : [ItemCore] = []
+        var tableItems : [ItemCore] = []
         let item = ItemCore(context: self.getContext())
         
         item.checked = true
@@ -122,17 +167,17 @@ class CoreDataManager {
         let entity = NSEntityDescription.entity(forEntityName: "ItemCore" , in: context)
         
         // let newtask1   = NSManagedObject(entity: entity!, insertInto: context ) as! ItemCore
-            let newtask1   = ItemCore(entity: entity!, insertInto: context )
+        let newtask1   = ItemCore(entity: entity!, insertInto: context )
         newtask1.setValue("Finir le cours d'IOS", forKey: "text")
         
-            let newtask2   = NSManagedObject(entity: entity!, insertInto: context ) as! ItemCore
+        let newtask2   = NSManagedObject(entity: entity!, insertInto: context ) as! ItemCore
         newtask2.setValue("Mettre à jour XCode", forKey: "text")
         newtask2.setValue(true, forKey: "checked")
         
-            let newtask3   = NSManagedObject(entity: entity!, insertInto: context )as! ItemCore
+        let newtask3   = NSManagedObject(entity: entity!, insertInto: context )as! ItemCore
         newtask3.setValue("Ma tache perso", forKey: "text")
         
-            let newtask4   = NSManagedObject(entity: entity!, insertInto: context ) as! ItemCore
+        let newtask4   = NSManagedObject(entity: entity!, insertInto: context ) as! ItemCore
         newtask4.setValue("Home work Java", forKey: "text")
         newtask4.setValue(true, forKey: "checked")
         
@@ -161,9 +206,9 @@ class CoreDataManager {
         
     }
     
-  
     
-        
+    
+    
     
     func getItems(predicate: NSPredicate? = nil,
                   sortDescriptors: [NSSortDescriptor]? = nil,
@@ -171,6 +216,7 @@ class CoreDataManager {
         
         
         let fetchRequest = NSFetchRequest<ItemCore>()
+        
         
         fetchRequest.predicate = predicate
         
@@ -180,17 +226,65 @@ class CoreDataManager {
         
     }
     
-        func  deleteItem(item: ItemCore){
-            getContext().delete(item)
-            saveData()
-        }
+    func getItemsFromCat(category: Category,
+                  sortDescriptors: [NSSortDescriptor]? = nil,
+                  limit: Int = 100) -> [ItemCore] {
         
-        func  addItem(item: ItemCore){
-            getContext().insert(item)
-            saveData()
-        }
+        let tableItems : [ItemCore] = []
         
+        let fetchRequest = NSFetchRequest<ItemCore>()
         
+        let predicate = NSPredicate(format: "withCat = %@", category)
+        
+        fetchRequest.predicate = predicate
+        
+        fetchRequest.sortDescriptors = sortDescriptors
+        
+        return try! getContext().fetch(fetchRequest)
+        
+    }
+    
+    func getItemFromCat(category: Category,
+                         sortDescriptors: [NSSortDescriptor]? = nil,
+                         limit: Int = 100) -> [ItemCore] {
+        
+        let tableItems : [ItemCore] = []
+        
+        let fetchRequest = NSFetchRequest<ItemCore>()
+        
+        let predicate = NSPredicate(format: "withCat = %@", category)
+        
+        fetchRequest.predicate = predicate
+        
+        fetchRequest.sortDescriptors = sortDescriptors
+        
+        return try! getContext().fetch(fetchRequest)
+        
+    }
+    
+    
+    func  deleteItem(item: ItemCore){
+        getContext().delete(item)
+        saveData()
+    }
+    func  deleteCategory(item: Category){
+        getContext().delete(item)
+        saveData()
+    }
+    
+    
+    func  addItem(item: ItemCore){
+        getContext().insert(item)
+        saveData()
+    }
+    
+    func addCategory(item: Category){
+        getContext().insert(item)
+        saveData()
+    }
+    
+    
+    
     func checkifCoreDataExists(text : String) -> Bool{
         //predicate // Use NSPredicate to filter articlID in coredata
         
@@ -255,6 +349,117 @@ class CoreDataManager {
         
     }
     
+    func initTableImg() -> [UIImage] {
+        var tableImages: [UIImage] = []
+        let img = UIImage(named: "audit-3167425_1920")
+        let img2 = UIImage(named: "cooking-2065142_1920")
+        let img3 = UIImage(named: "education-1545578_1280")
+        tableImages.append(img!)
+        tableImages.append(img2!)
+        tableImages.append(img3!)
+        
+        return tableImages
+    }
+    
+    func initTableSections() ->[String] {
+        
+        var tablesections : [String] = []
+        let str1: String = "Simple Tasks"
+        let str2: String = "Urgent Tasks"
+        let str3: String = "Must do it Tasks"
+        tablesections.append(str1)
+        tablesections.append(str2)
+        tablesections.append(str3)
+        return tablesections
+        
+    }
+    
+    func deleteAllItems(){
+        
+        getContext().deletedObjects
+        //getContext().reset()
+        saveData()
+        
+    }
+    
+    func initTableSectionsCoredata(){
+        
+        var tableCategories : [Category] = []
+        
+        let newcat1   = Category(context: self.getContext())
+        newcat1.name = "Simple Task"
+        
+//        var dataImage : Data = UIImagePNGRepresentation(UIImage(named: "cooking-2065142_1920")!)!
+//        newcat1.setValue(dataImage, forKey: "image")
+//
+//
+//        let newcat2   = Category(entity: entityCat!, insertInto: context )
+//        newcat2.setValue("Urgent Tasks", forKey: "name")
+//        dataImage = UIImagePNGRepresentation(UIImage(named: "audit-3167425_1920")!)!
+//        newcat2.setValue(dataImage, forKey: "image")
+//
+//
+//
+//        let newcat3   = Category(entity: entityCat!, insertInto: context )
+//        newcat3.setValue("Must do it Tasks", forKey: "name")
+//        dataImage = UIImagePNGRepresentation(UIImage(named: "education-1545578_1280")!)!
+//        newcat3.setValue(dataImage, forKey: "image")
+//
+//        //Item entity
+//
+//        let newtask1   = ItemCore(entity: entityItem!, insertInto: context )
+//        newtask1.setValue("Finir le cours d'IOS", forKey: "text")
+//
+//
+//        let newtask2   = NSManagedObject(entity: entityItem!, insertInto: context ) as! ItemCore
+//        newtask2.setValue("Mettre à jour XCode", forKey: "text")
+//        newtask2.setValue(true, forKey: "checked")
+//
+//        let newtask3   = NSManagedObject(entity: entityItem!, insertInto: context )as! ItemCore
+//        newtask3.setValue("Ma tache perso", forKey: "text")
+//
+//        let newtask4   = NSManagedObject(entity: entityItem!, insertInto: context ) as! ItemCore
+//        newtask4.setValue("Home work Java", forKey: "text")
+//        newtask4.setValue(true, forKey: "checked")
+//
+//        newcat1.addToWithItem(newtask1)
+//        newcat2.addToWithItem(newtask2)
+//        newcat2.addToWithItem(newtask3)
+//        newcat3.addToWithItem(newtask4)
+//
+//        saveData()
+//
+//        //TODO insert into Coredata BBD
+//        //Recoup of context from CoreDataManager
+//        //let test = ItemCore(context: NSManagedObjectContext())
+//
+//        tableCategories.append(newcat1 )
+//        tableCategories.append(newcat2 )
+//        tableCategories.append(newcat3 )
+        
+        
+        self.saveData ()
+    }
+    
+    
+    func fetchCoreData_fromContext_Category() -> [Category]{
+        
+        var tableCategories : [Category] = []
+        
+        let request : NSFetchRequest<Category> = Category.fetchRequest()
+        
+        do {
+            let result =  try  self.getContext().fetch(request)
+            
+            tableCategories = result
+            
+        } catch  {
+            print("Fetch Failed")
+        }
+        return tableCategories
+        
+    }
+    
     
     func fetchCoreData_fromContext() -> [ItemCore]{
         
@@ -262,23 +467,16 @@ class CoreDataManager {
         
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "ItemCore")
         
-      
+        
         
         do {
             let result =  try  self.getContext().fetch(request)  // context contient the same data which  array [ItemCore]
             for data in result as! [NSManagedObject]{
                 
-                print(data.value(forKey: "text" ) as! String)
-                
-                
-               // let  _txt: String = data.value(forKey: "text" ) as! String
-               // let  _checked     = data.value(forKey: "checked" ) as? Bool
-               // let  _photo           = data.value(forKey: "photo" ) as? NSData
+    
                 
                 tableItems.append(data as! ItemCore)
-                //newrecord.setValue(_txt, forKey: "text") // EBE  comment recup Key generique ??
-                //newrecord.setValue(_checked, forKey: "checked")
-                //newrecord.setValue(_photo, forKey: "photo")
+               
                 
             }
             
@@ -289,7 +487,120 @@ class CoreDataManager {
         return tableItems
     }
     
-        func fetchfromCoreData2() -> [ItemCore]{
+    func fetchCoreData_fromContext(category: Category ) -> [ItemCore]{
+        
+        var tableItems : [ItemCore] = []
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "ItemCore")
+        
+        //let fetchRequest = NSFetchRequest<ItemCore>()
+        
+        let predicate = NSPredicate(format: "ANY withCat = %@", category)
+       // let predicate = NSPredicate(format: "withCat = %@", NSSet(object: category))
+       // let predicate = NSPredicate(format: "ANY withCat = %@", NSSet(object: category))
+       //  request.predicate = predicate
+       //fetchRequest.predicate = predicate
+       // fetchRequest.sortDescriptors = sortDescriptors
+        
+        do {
+            let result =  try  self.getContext().fetch(request)  // context contient the same data which  array [ItemCore]
+            for data in result as! [NSManagedObject]{
+             let dataCast = data as! ItemCore
+               if(dataCast.withCat?.objectID == category.objectID){
+                      tableItems.append(data as! ItemCore)
+                }
+              
+            }
+            
+        } catch  {
+            print("Fetch Failed")
+        }
+        
+        return tableItems
+    }
+    
+    func deleteItemCore_fromCategory(item: ItemCore,category: Category ){
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "ItemCore")
+        
+        do {
+            let result =  try  self.getContext().fetch(request)  // context contient the same data which  array [ItemCore]
+            for data in result as! [NSManagedObject]{
+                let dataCast = data as! ItemCore
+                
+                if(dataCast.objectID.isEqual(item.objectID) && (dataCast.withCat?.objectID.isEqual(category.objectID))!){
+                    deleteItem(item: data as! ItemCore)
+                }
+                
+            }
+            
+        } catch  {
+            print("Fetch Failed")
+        }
+     
+    }
+    
+    func deleteCategoryIfHasNoItems(category: Category ) -> Bool{
+        
+        var ret: Bool = false
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Category")
+        
+        do {
+            let result =  try  self.getContext().fetch(request)  // context contient
+            
+            for data in result as! [NSManagedObject]{
+                let dataCast = data as! Category
+                
+                
+                if(dataCast.objectID.isEqual(category.objectID) && (dataCast.withItem?.allObjects.count == 0)){
+                    self.deleteCategory(item: category)
+                    return true
+                }
+                
+            }
+           
+            
+        } catch  {
+            print("Fetch Failed")
+        }
+         return ret
+    }
+    
+                                    // self ==
+    // let predicate = NSPredicate(format: "self IN %@", objectIDs)
+    func updateItemFromCategory(category: Category ,itemEdited: ItemCore) -> Bool{
+        
+        var ret: Bool = false
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "ItemCore")
+        
+        do {
+            let result =  try  self.getContext().fetch(request)  // context contient
+            
+            for data in result as! [NSManagedObject]{
+                let dataCast = data as! ItemCore
+                
+                
+                if(dataCast.objectID.isEqual(itemEdited.objectID) && (dataCast.withCat?.objectID.isEqual(category.objectID))!){
+                    dataCast.checked = itemEdited.checked
+                    dataCast.deadline = itemEdited.deadline
+                    dataCast.photo = itemEdited.photo
+                    dataCast.text = itemEdited.text
+                    saveData ()
+                    return true
+                }
+                
+            }
+            
+            
+        } catch  {
+            print("Fetch Failed")
+        }
+        return ret
+    }
+    
+    func fetchfromCoreData2() -> [ItemCore]{
         
         var tableItems : [ItemCore] = []
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "ItemCore")
@@ -313,7 +624,7 @@ class CoreDataManager {
                     newtask.setValue(true, forKey: "checked")
                     
                     tableItems.append(newtask)
-                    saveData () // Not sure if need it here
+                    saveData ()
                     
                 }
                 
@@ -326,12 +637,12 @@ class CoreDataManager {
         
     }
     
-  
+    
     
     
     // MARK: - C
     
     
- 
-  }
+    
+}
 
